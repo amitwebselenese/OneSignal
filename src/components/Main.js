1e1ef502ddd062749a4c687142b969d4dcef5f94
Amit Toledano <amit@webselenese.com>
2021-05-05 10:40:47 +0300

    create new app

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Main.css";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <div className="header">WebSelenese Test Site</div>
+    </div>
+  );
+};
+
+export default Main;
